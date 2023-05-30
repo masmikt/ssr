5b8d31d82471f4cf5ffdd32e615dc25dbc3fdec0
masmik <masmik@weareclario.com>
2023-05-30 18:38:15 +0300

    main test woff fonts

--- src/app/(mobile)/(landings)/setup-flow/page.tsx
+++ src/app/(mobile)/(landings)/setup-flow/page.tsx
@@ -4,6 +4,13 @@ import dynamic from 'next/dynamic'
 import { Loader } from '@/app/(common)/components/loader';
 
 import { IntroSection } from '@/app/(mobile)/(landings)/setup-flow/components/intro';
+import { FixedHeaderLayout } from '@/app/(common)/contexts';
+import { LayoutContainer } from '@/app/(common)/components/layoutContainer';
+import { ContentWrapper } from '@/app/(common)/components/contentWrapper';
+import { MobileHeader } from '@/app/(mobile)/components/mobileHeader';
+import css from './layout.module.scss';
+import { FixedHeader } from '@/app/(mobile)/(landings)/setup-flow/components/fixedHeader';
+import { CookiesBanner } from '@/app/(common)/components/cookiesBanner';
 
 const Awards = dynamic(() => import('@/app/(mobile)/(landings)/setup-flow/components/awards/awards'));
 const TrustedBy = dynamic(() => import('@/app/(mobile)/(landings)/setup-flow/components/trustedBy/trustedBy'));
@@ -21,20 +28,33 @@ const Footer = dynamic(() => import('@/app/(mobile)/(landings)/setup-flow/compon
 export default async function SetupFlow() {
     return (
         <Suspense fallback={<Loader />}>
-            <main>
-                <IntroSection />
-                <Awards />
-                <TrustedBy />
-                <FeaturesInfo />
-                <Support />
-                <InstructionsList />
-                <UserRate />
-                <Pricing />
-                <ProtectDevices />
-                <Comments />
-                <StopSpied />
-                <Footer />
-            </main>
+            <FixedHeaderLayout>
+                <LayoutContainer>
+                    <ContentWrapper>
+                        <MobileHeader className={css['layout__header']} />
+                    </ContentWrapper>
+                    <FixedHeader />
+                    <div className={css['layout__content']}>
+                        <div className={css['layout__content']}>
+                            <main>
+                                <IntroSection />
+                                <Awards />
+                                <TrustedBy />
+                                <FeaturesInfo />
+                                <Support />
+                                <InstructionsList />
+                                <UserRate />
+                                <Pricing />
+                                <ProtectDevices />
+                                <Comments />
+                                <StopSpied />
+                                <Footer />
+                            </main>
+                        </div>
+                    </div>
+                    <CookiesBanner />
+                </LayoutContainer>
+            </FixedHeaderLayout>
         </Suspense>
     );
 }
