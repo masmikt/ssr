e7033f4a9a6d174f27cd36c04286d44561f0df91
masmik <masmik@weareclario.com>
2023-05-30 17:57:28 +0300

    main test without segment

--- src/app/(common)/shared/hooks/useSendEvent.ts
+++ src/app/(common)/shared/hooks/useSendEvent.ts
@@ -1,60 +1,61 @@
-import { objectOmitNull } from '../helpers';
-import { InterstellarProductId, TrackingParams } from '../constants';
-import { useTracking } from './useTracking';
+// import { objectOmitNull } from '../helpers';
+// import { InterstellarProductId, TrackingParams } from '../constants';
+// import { useTracking } from './useTracking';
 import { ICustomEventParams, ISegmentEvent, useAnalytics } from '@/app/(common)/contexts/analyticsProvider';
-import { usePathname } from 'next/navigation';
+// import { usePathname } from 'next/navigation';
 
 export const useSendEvent = () => {
-    const { send } = useAnalytics();
-    const pathname = usePathname();
-    const {
-        getFbp,
-        getFbc,
-        getTrackingParam,
-        getGaCid,
-        getIteration,
-        getTypeDevice,
-        getMarketing,
-        getUserAgent
-    } = useTracking();
-    const pageName = pathname.slice(1);
+    // const { send } = useAnalytics();
+    // const pathname = usePathname();
+    // const {
+    //     getFbp,
+    //     getFbc,
+    //     getTrackingParam,
+    //     getGaCid,
+    //     getIteration,
+    //     getTypeDevice,
+    //     getMarketing,
+    //     getUserAgent
+    // } = useTracking();
+    // const pageName = pathname.slice(1);
 
-    const prepareEventMessage = (
-        eventName: string,
-        params?: ICustomEventParams
-    ): ISegmentEvent => {
-        const typeDevice = getTypeDevice();
-        return objectOmitNull(
-            {
-                eventName,
-                properties: {
-                    $os: typeDevice,
-                    affid: getTrackingParam(TrackingParams.Affid),
-                    gaCid: getGaCid(),
-                    pageName: /^\//.test(pageName) ? pageName : `${pageName}`,
-                    marketing: getMarketing(),
-                    fbclid: getTrackingParam(TrackingParams.Fbclid),
-                    fbp: getFbp(),
-                    fbc: getFbc(),
-                    prodId: `${InterstellarProductId}`,
-                    obClickId: getTrackingParam(TrackingParams.ObClickId),
-                    typeDevice,
-                    iteration: getIteration(),
-                    cbt: getTrackingParam(TrackingParams.Cbt),
-                    userAgent: getUserAgent(),
-                    ...params,
-                },
-            },
-            false
-        ) as ISegmentEvent;
-    };
+    // const prepareEventMessage = (
+    //     eventName: string,
+    //     params?: ICustomEventParams
+    // ): ISegmentEvent => {
+    //     const typeDevice = getTypeDevice();
+    //     return objectOmitNull(
+    //         {
+    //             eventName,
+    //             properties: {
+    //                 $os: typeDevice,
+    //                 affid: getTrackingParam(TrackingParams.Affid),
+    //                 gaCid: getGaCid(),
+    //                 pageName: /^\//.test(pageName) ? pageName : `${pageName}`,
+    //                 marketing: getMarketing(),
+    //                 fbclid: getTrackingParam(TrackingParams.Fbclid),
+    //                 fbp: getFbp(),
+    //                 fbc: getFbc(),
+    //                 prodId: `${InterstellarProductId}`,
+    //                 obClickId: getTrackingParam(TrackingParams.ObClickId),
+    //                 typeDevice,
+    //                 iteration: getIteration(),
+    //                 cbt: getTrackingParam(TrackingParams.Cbt),
+    //                 userAgent: getUserAgent(),
+    //                 ...params,
+    //             },
+    //         },
+    //         false
+    //     ) as ISegmentEvent;
+    // };
 
     const sendEvent = async (
         eventName: string,
         eventParams?: ICustomEventParams
     ): Promise<void> => {
-        const eventMessage = prepareEventMessage(eventName, eventParams);
-        send?.(eventMessage);
+        console.log(`!!! SEND EVENT`);
+        // const eventMessage = prepareEventMessage(eventName, eventParams);
+        // send?.(eventMessage);
     };
 
     return {
