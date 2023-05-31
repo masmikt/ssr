ca866c6227120b170e40d951adf2c723c30ebec4
masmik <masmik@weareclario.com>
2023-06-01 00:16:40 +0300

    test perf

--- src/app/(mobile)/(landings)/setup-flow/components/presentationVideo/presentationVideo.tsx
+++ src/app/(mobile)/(landings)/setup-flow/components/presentationVideo/presentationVideo.tsx
@@ -1,17 +1,14 @@
 import css from './presentationVideo.module.scss';
-import dynamic from 'next/dynamic'
 
-import PresentationVideoImage from './presentationVideoImage';
 
 
 const PresentationVideo = () => {
     return (
         <div className={css["presentation-video"]}>
-            <ReactPlayer
-                url={"videos/setupFlow/setup-intro-opt.mp4"}
-                loop playing muted playsinline width="272px"
-                fallback={<PresentationVideoImage />}
-                height="304px" />
+            <video autoPlay loop muted>
+                <source src={"videos/setupFlow/setup-intro-opt.mp4"} type="video/mp4" />
+                <img src={'public/images/mobile/landings/setup-flow/find-spyware-video-img.webp'} />
+            </video>
         </div>
     )
 }
