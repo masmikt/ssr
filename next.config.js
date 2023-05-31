bdae25780a3b1ec7e63f27da67ce4ba25774b130
masmik <masmik@weareclario.com>
2023-05-31 23:24:55 +0300

    test perf

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: '/',
-    //             destination: '/setup-flow',
-    //             permanent: false,
-    //         },
-    //     ];
-    // },
     output: 'standalone',
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { FixedHeaderLayout } from '@/app/(common)/contexts';
+import { LayoutContainer } from '@/app/(common)/components/layoutContainer';
+
 export default function Home() {
-    return <>Home page</>;
+    return (
+        <FixedHeaderLayout>
+            <LayoutContainer>
+                <>Home page</>
+            </LayoutContainer>
+        </FixedHeaderLayout>);
 }
