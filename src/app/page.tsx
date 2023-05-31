b709d9fd2d6d5ff5b59c982816f5664157110ad6
masmik <masmik@weareclario.com>
2023-06-01 00:44:29 +0300

    test perf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,34 @@
 import css from './layout.module.scss';
-import { FixedHeaderLayout } from '@/app/(common)/contexts';
+import clsx from 'clsx';
 import { LayoutContainer } from '@/app/(common)/components/layoutContainer';
 import { ContentWrapper } from '@/app/(common)/components/contentWrapper';
-import { MobileHeader } from '@/app/(mobile)/components/mobileHeader';
-import { IntroSection } from '@/app/(mobile)/(landings)/setup-flow/components/intro';
-// import dynamic from 'next/dynamic';
-
-// const FixedHeader = dynamic(() => import("@/app/(mobile)/(landings)/setup-flow/components/fixedHeader/fixedHeader"), {
-//     ssr: false,
-// });
+import { Typography, TypographyComponents, TypographyVariants } from '@/app/(common)/components/typography';
 
 export default function Home() {
     return (
-        // <FixedHeaderLayout>
         <LayoutContainer>
             <ContentWrapper>
-                {/*<MobileHeader className={css['layout__header']} />*/}
             </ContentWrapper>
-            {/*<FixedHeader />*/}
             <div className={css['layout__content']}>
                 <div className={css['layout__content']}>
                     <main>
                         <>Home page</>
-                        <IntroSection />
+                        <Typography
+                            variant={TypographyVariants.h2}
+                            component={TypographyComponents.div}
+                            className={css['intro__title']}>
+                            Stop your
+                            <span className={css['intro__title--highlighted']}>{` phone from being spied on.`}</span>
+                        </Typography>
+                        <Typography
+                            variant={TypographyVariants.p2}
+                            component={TypographyComponents.p}
+                            className={clsx(css['intro__text'], 'mt-xs')}>
+                            Try Clario, a smart anti-spy app that effectively protects your privacy.
+                        </Typography>
                     </main>
                 </div>
             </div>
         </LayoutContainer>
-        // </FixedHeaderLayout>
     );
 }
