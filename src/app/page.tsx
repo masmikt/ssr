4a15c105401cfb0a2a83c761474e258efe25e1d3
masmik <masmik@weareclario.com>
2023-05-31 23:28:59 +0300

    test perf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+import css from './layout.module.scss';
 import { FixedHeaderLayout } from '@/app/(common)/contexts';
 import { LayoutContainer } from '@/app/(common)/components/layoutContainer';
+import { ContentWrapper } from '@/app/(common)/components/contentWrapper';
+import { MobileHeader } from '@/app/(mobile)/components/mobileHeader';
+import dynamic from 'next/dynamic';
+
+const FixedHeader = dynamic(() => import("@/app/(mobile)/(landings)/setup-flow/components/fixedHeader/fixedHeader"), {
+    ssr: false,
+});
 
 export default function Home() {
     return (
         <FixedHeaderLayout>
             <LayoutContainer>
-                <>Home page</>
+                <ContentWrapper>
+                    <MobileHeader className={css['layout__header']} />
+                </ContentWrapper>
+                <FixedHeader />
+                <div className={css['layout__content']}>
+                    <div className={css['layout__content']}>
+                        <main>
+                            <>Home page</>
+                        </main>
+                    </div>
+                </div>
             </LayoutContainer>
         </FixedHeaderLayout>);
 }
