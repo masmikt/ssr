a9cc10a0a832715909fdadfa8e167fe7204b0389
masmik <masmik@weareclario.com>
2023-06-01 00:18:58 +0300

    test perf

--- src/app/(mobile)/(landings)/setup-flow/components/presentationVideo/presentationVideo.tsx
+++ src/app/(mobile)/(landings)/setup-flow/components/presentationVideo/presentationVideo.tsx
@@ -1,13 +1,12 @@
 import css from './presentationVideo.module.scss';
 
 
-
 const PresentationVideo = () => {
     return (
         <div className={css["presentation-video"]}>
             <video autoPlay loop muted>
-                <source src={"videos/setupFlow/setup-intro-opt.mp4"} type="video/mp4" />
-                <img src={'public/images/mobile/landings/setup-flow/find-spyware-video-img.webp'} />
+                <source src={"videos/setupFlow/setup-intro-opt.mp4"} width="272px" height="304px" type="video/mp4" />
+                <img src={'images/mobile/landings/setup-flow/find-spyware-video-img.webp'} />
             </video>
         </div>
     )
