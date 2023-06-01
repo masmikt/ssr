962e4c5a1bebd3fbf5922c57e3b597a4d6b7e6ea
masmik <masmik@weareclario.com>
2023-06-01 14:08:10 +0300

    main speed up

---
+++ src/app/(mobile)/(landings)/speed/page.tsx
@@ -0,0 +1,10 @@
+import { LightFacebookPixel } from '@/app/(common)/components/lightFacebookPixel';
+
+
+export default async function Speed() {
+    return (
+        <>
+            <LightFacebookPixel />
+        </>
+    );
+}
